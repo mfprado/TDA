6fbe3cc86cdb825ec366252a81b1489771d528d7
mariaflorenciaprado <pradomflorencia@gmail.com>
2018-04-25 02:13:51 -0300

    comparacion s1 s2

--- TP.2/Parte2/rotacionCiclicaFB.py
+++ TP.2/Parte2/rotacionCiclicaFB.py
@@ -2,7 +2,7 @@ def esRotacionCiclicaFB(s1, s2) :
     rotacionPrevia = s2
     largo = len(s2) - 1
     for i in range(0,len(s2)) :
-        if s2 == (rotacionPrevia[largo] + rotacionPrevia[0 : largo]) :
+        if s1 == (rotacionPrevia[largo] + rotacionPrevia[0 : largo]) :
             return True
         rotacionPrevia = (rotacionPrevia[largo] + rotacionPrevia[0 : largo])
     return False
