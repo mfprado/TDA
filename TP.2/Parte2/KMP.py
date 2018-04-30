529c736cc14ae8fa068f2486c519fc7f2fdba809
mariaflorenciaprado <pradomflorencia@gmail.com>
2018-04-30 19:21:45 -0300

    implementado KMP

--- TP.2/Parte2/KMP.py
+++ TP.2/Parte2/KMP.py
@@ -1,36 +1,37 @@
 def KMPTable(pattern):
-    # Create the resulting table, which for length zero is None.
     result = [None]
-
-    # Iterate across the rest of the characters, filling in the values for the
-    # rest of the table.
     for i in range(0, len(pattern)):
-        # Keep track of the size of the subproblem we're dealing with, which
-        # starts off using the first i characters of the string.
         j = i
-
         while True:
-            # If j hits zero, the recursion says that the resulting value is
-            # zero since we're looking for the LPB of a single-character
-            # string.
             if j == 0:
                 result.append(0)
                 break
-
-            # Otherwise, if the character one step after the LPB matches the
-            # next character in the sequence, then we can extend the LPB by one
-            # character to get an LPB for the whole sequence.
             if pattern[result[j]] == pattern[i]:
                 result.append(result[j] + 1)
                 break
-
-            # Finally, if neither of these hold, then we need to reduce the
-            # subproblem to the LPB of the LPB.
             j = result[j]
-
     return [-1] + result[1:len(result)-1]
-
-
 
 print (KMPTable("ABRACADABRA"))
 print (KMPTable("DABRAABRACA"))
+
+def KMP(pattern, text):
+    tf = KMPTable(pattern)
+    k = 0
+    i = 0
+    while k + i < len(text):
+        print (k, i)
+        if text[k + i] == pattern[i]:
+            i = i + 1
+            if k == len(pattern):
+                return k
+        else:
+            if i == 0:
+                k = k + 1
+            else:
+                k = k + i - tf[i]
+                i = tf[i]
+    return None
+
+
+print(KMP('ABRACADABRA','ABRACADAPA ABRACADABRA'))
