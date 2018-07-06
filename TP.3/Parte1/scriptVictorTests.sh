afa8fd0b0456b762a92ee0be452036007641d632
mariaflorenciaprado <pradomflorencia@gmail.com>
2018-07-06 18:49:13 -0300

    script victor tests

---
+++ TP.3/Parte1/scriptVictorTests.sh
@@ -0,0 +1,7 @@
+python Main.py victorTests/bNaval_uniforme4x4.txt 2 GREEDO
+python Main.py victorTests/bNaval_uniforme4x4_difVida.txt 2 GREEDO
+python Main.py victorTests/bNaval_random5x4_0_100.txt 3 GREEDO
+python Main.py victorTests/bNaval_random4x4_0_20.txt 3 GREEDO
+python Main.py victorTests/bNaval_pocodenso16x4.txt 4 GREEDO
+python Main.py victorTests/bNaval_gradientes_invertidos_4x5.txt 2 GREEDO
+python Main.py victorTests/bNaval_mas_de_uno.txt 2 GREEDO
