183e3704f99e7523e766864dc51b9ee1a2abd48f
mariaflorenciaprado <pradomflorencia@gmail.com>
2018-07-08 19:38:15 -0300

    main del error

--- TP.3/Parte1/Main.py
+++ TP.3/Parte1/Main.py
@@ -2,15 +2,19 @@ import sys
 from Game import *
 
 if __name__ == '__main__':
-    boardPath = sys.argv[1]
-    shuttlesCount = sys.argv[2]
-    strategy = sys.argv[3]
+    # boardPath = sys.argv[1]
+    # shuttlesCount = sys.argv[2]
+    # strategy = sys.argv[3]
+    #
+    boardPath = "victorTests/bNaval_random5x4_0_100.txt"
+    shuttlesCount = 3
+    strategy = "DINAMICO"
     game = Game(boardPath, int(shuttlesCount), strategy)
     points = 0
     turn = 1
     print("\n\x1b[5;30;41mJUGANDO CON ESTRATEGIA: " + strategy + "\x1b[0m\n")
     while not game.finished():
-        print ("TURNO NUMERO: " + str(turn))
+        print ("\nTURNO NUMERO: " + str(turn))
         points += game.playTurn()
         print ("PUNTOS : " + str(points))
         print ("TURNOS TOTALES: " + str(turn))
@@ -20,3 +24,4 @@ if __name__ == '__main__':
     f = open('output.txt', 'a')
     f.write("Tablero: " + str(boardPath) + " - Con Lanzaderas: " + str(shuttlesCount) + "\nModo: " + str(strategy) +    " - Puntos: " + str(points) + "\n")
     f.close()
+
--- TP.3/Parte1/scriptVictorTests.sh
+++ TP.3/Parte1/scriptVictorTests.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "RESULTADOS DE LOS TESTS" > output.txt
-python Main.py victorTests/bNaval_uniforme4x4.txt 2 DINAMICO
+#python Main.py victorTests/bNaval_uniforme4x4.txt 2 DINAMICO
 #python Main.py victorTests/bNaval_uniforme4x4.txt 2 GREEDO
 #python Main.py victorTests/bNaval_uniforme4x4_difVida.txt 2 DINAMICO
 #python Main.py victorTests/bNaval_uniforme4x4_difVida.txt 2 GREEDO
-#python Main.py victorTests/bNaval_random5x4_0_100.txt 3 DINAMICO
+python Main.py victorTests/bNaval_random5x4_0_100.txt 3 DINAMICO
 #python Main.py victorTests/bNaval_random5x4_0_100.txt 3 GREEDO
 #python Main.py victorTests/bNaval_random4x4_0_20.txt 3 DINAMICO
 #python Main.py victorTests/bNaval_random4x4_0_20.txt 3 GREEDO
